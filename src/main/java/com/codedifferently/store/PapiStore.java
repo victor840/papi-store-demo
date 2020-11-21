0add1c851353720dd4addc618d2006fa596a1534
kelechi victor <victor.ugoanusi@gmail.com>
2020-11-21 12:55:39 -0500

    Added comments

--- src/main/java/com/codedifferently/store/PapiStore.java
+++ src/main/java/com/codedifferently/store/PapiStore.java
@@ -1,39 +1,49 @@
+//What package we are inside of.
 package com.codedifferently.store;
 
+//classes we are importing. This way we can use ArrayLists, HashMaps & Map.
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+//Create a public class named Papistore.
+// Add private variables which will be our "State" once we create an object from this class.
 public class PapiStore {
     private boolean isStoreOpen;
     private ArrayList<String> tabs;
+    //Create a Map which will hold a name and an amount owed.
     Map<String, Double> owesPapiMoney;
 
+    //constructor function that sets up our "State" variables values.
     public PapiStore(){
-        this.isStoreOpen = false;
-        this.tabs = new ArrayList();
-        this.owesPapiMoney = new HashMap<>();
+        this.isStoreOpen = false; //initially set store to closed.
+        this.tabs = new ArrayList(); // ArrayList of tabs
+        this.owesPapiMoney = new HashMap<>(); // Map of who owes, and how much they owe.
     }
 
+    //Second constructor function. This one we can pass in values - we can start the store as open.
     public PapiStore(Boolean isOpen, ArrayList<String> tabsIn){
         this.isStoreOpen = isOpen;
         this.tabs = tabsIn;
         this.owesPapiMoney = new HashMap<>();
     }
 
+    // This will allow us to close the store.
     public void closeStore(){
 
     }
 
+    //this will allow us to open the store, if its already open it wont change anything.
     public void openStore(){
         this.isStoreOpen = true;
     }
 
+    //tell us if the store is open.
     public boolean isStoreOpen() {
         return isStoreOpen;
     }
 
-
+    //tell us if this person is allowed to have a tab. If they have an overdue balance it will return false. Else true.
     public Boolean allowToUseTab(String name){
         Boolean response = false;
         if(hasAnActiveTab(name) && !hasAnOverDueBalance(name))
@@ -41,6 +51,7 @@ public class PapiStore {
         return response;
     }
 
+    // A private method for us to check if our name has an overdue balance. Will return true or false.
     private boolean hasAnOverDueBalance(String name){
         if(!owesPapiMoney.containsKey(name))
             return false;
@@ -51,6 +62,7 @@ public class PapiStore {
         return owes;
     }
 
+    // A private method to tell us if this person has an active tab. Will return true or false.
     private boolean hasAnActiveTab(String name){
         for (String tab : tabs){
             if (tab.equals(name))
@@ -59,11 +71,12 @@ public class PapiStore {
         return false;
     }
 
+    //will create an active tab
     public void createActiveTab(String name){
         this.tabs.add(name);
     }
 
-
+    // print out welcome message.
     public static void main(String[] args) {
         System.out.println("Welcome to Papi Store, Hola");
     }
--- src/test/java/com/codedifferently/store/PapiStoreTest.java
+++ src/test/java/com/codedifferently/store/PapiStoreTest.java
@@ -1,63 +1,71 @@
 // This is the package it defines the family relation of objects
 package com.codedifferently.store;
 
-// This is importing the Assert Class from the org.junit package or folder
+// This is importing Classes from the org.junit package/folder
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 // This is a public class called Papi
-    public class PapiStoreTest {
+public class PapiStoreTest {
 
-        PapiStore papiStore;
+    // creates a reference papiStore of object PapiStore.
+    PapiStore papiStore;
 
-        @Before
-        public void setup(){
-            papiStore = new PapiStore();
-            papiStore.createActiveTab("Jamal");
-            papiStore.createActiveTab("Victor");
-            papiStore.owesPapiMoney.put("Jamal", 0.0);
-            papiStore.owesPapiMoney.put("Victor", 7.0);
-        }
+    //fixture annotation
+    // sets papiStore reference to new PapiStore.
+    // the setup method assigning values.
+    @Before
+    public void setup(){
+        papiStore = new PapiStore();
+        papiStore.createActiveTab("Jamal");
+        papiStore.createActiveTab("Victor");
+        papiStore.owesPapiMoney.put("Jamal", 0.0);
+        papiStore.owesPapiMoney.put("Victor", 7.0);
+    }
 
-        @Test
-        public void OpenStoreTest(){
-            //Given
+    //Test fixture annotation runs test.
+    //Checking to see if store is open.
+    @Test
+    public void OpenStoreTest(){
+        //Given
 
-            papiStore.openStore();
+        papiStore.openStore();
 
-            //When
-            Boolean expected = true;
-            Boolean actual = papiStore.isStoreOpen();
+        //When
+        Boolean expected = true;
+        Boolean actual = papiStore.isStoreOpen();
 
-            //Then
-            //Assert.assertEquals(expected, actual);
-            Assert.assertTrue(actual);
-        }
+        //Then
+        //Assert.assertEquals(expected, actual);
+        Assert.assertTrue(actual);
+    }
 
-        @Test
-        public void allowToUseTabTestSuccess(){
-            // given
-            PapiStore papiStore = new PapiStore();
+    //Checking to see if person can use tab.
+    @Test
+    public void allowToUseTabTestSuccess(){
+        // given
+        PapiStore papiStore = new PapiStore();
 
-            // when
-            Boolean expected = true;
-            Boolean actual = papiStore.allowToUseTab("Jamal");
+        // when
+        Boolean expected = true;
+        Boolean actual = papiStore.allowToUseTab("Jamal");
 
-            // then
-            Assert.assertEquals(expected, actual);
-        }
+        // then
+        Assert.assertEquals(expected, actual);
+    }
 
-        @Test
-        public void allowToUseTabTestFail(){
-            // given
-            PapiStore papiStore = new PapiStore();
+    //Checking to see if person cannot use tab.
+    @Test
+    public void allowToUseTabTestFail(){
+        // given
+        PapiStore papiStore = new PapiStore();
 
-            // when
-            Boolean expected = false;
-            Boolean actual = papiStore.allowToUseTab("Victor");
+        // when
+        Boolean expected = false;
+        Boolean actual = papiStore.allowToUseTab("Victor");
 
-            // then
-            Assert.assertEquals(expected, actual);
-        }
+        // then
+        Assert.assertEquals(expected, actual);
+    }
 }
